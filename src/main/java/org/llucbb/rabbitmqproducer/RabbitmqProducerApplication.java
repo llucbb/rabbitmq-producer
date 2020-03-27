37a25c0660991a42605e42004e41632b2f56d9a4
llucbb <llucbb@gmail.com>
2020-03-27 15:49:37 +0100

    First commit

---
+++ src/main/java/org/llucbb/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -0,0 +1,18 @@
+package org.llucbb.rabbitmqproducer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
